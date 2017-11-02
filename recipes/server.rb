939ac7b8cbab4ffd5c8bb28717ef4565c11a397a
ssrungaram <srungaram@hotmail.com>
2017-11-02 13:58:44 +0000

    0.2.1 embedded erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,9 +8,15 @@ powershell_script 'Install IIS' do
   action :run
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content 'Hello, world!'
-  rights :read, 'Everyone'
+# file 'C:\inetpub\wwwroot\Default.htm' do
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
+  # content "<h1>Hello, world!</h1>
+  # <h2>PLATFORM: #{node['platform']}</h2>
+  # <h2>HOSTNAME: #{node['hostname']}</h2>
+  # <h2>MEMORY: #{node['memory']}</h2>
+  # <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+  # rights :read, 'Everyone'
 end
 
 service 'w3svc' do
